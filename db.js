ddb607ebdf944026c3110aa8d5659d43702e81bf
Eklavya Bhargava <eklavya@orufy.com>
2024-02-16 21:02:59 +0530

    node module removed

--- db.js
+++ db.js
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
-
-require('dotenv').config();
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
-    const MONGODB_URL = process.env.MONGODB_URL;
+  const MONGODB_URL = process.env.MONGODB_URL;
 
-    try {
-        // connect to mongodb database
-        await mongoose.connect(MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected to MongoDB!');
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-}
+  try {
+    // connect to mongodb database
+    await mongoose.connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDb;
