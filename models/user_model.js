2da47bf520340df4421623cf294bb01beb00f554
Eklavya Bhargava <eklavya@orufy.com>
2024-03-30 12:35:52 +0530

    Update user_modal.js

--- models/user_model.js
+++ models/user_model.js
@@ -7,7 +7,11 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     emailId: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    profilePic: { type: String, default: "user.png" },
+    profilePic: {
+      type: String,
+      default:
+        "https://firebasestorage.googleapis.com/v0/b/twitter-clone-webapp-dev.appspot.com/o/public%2Fuser.png?alt=media&token=3d29f923-8b10-45a2-ae90-7ab5d51295b3",
+    },
     location: { type: String },
     dob: { type: Date },
     followers: [{ type: ObjectId, ref: "User" }],
